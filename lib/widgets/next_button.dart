7ec17e3516833dcacb4f00d436e0a41840bedb1c
22091397054_Salsabilla Athifah Yonara <124547235+sathipyn@users.noreply.github.com>
2024-04-05 10:25:59 +0700

    Update next_button.dart

---
+++ lib/widgets/next_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+
+class TombolNext extends StatelessWidget {
+  const TombolNext({
+    super.key,
+    required this.onPressed,
+    required this.label,
+  });
+
+  final VoidCallback? onPressed;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      child: SizedBox(
+        height: 50,
+        width: double.infinity,
+        child: Card(
+          color: onPressed != null ? Colors.white : null,
+          child: Center(
+            child: Text(
+              label,
+              style: const TextStyle(
+                letterSpacing: 2,
+                fontSize: 25,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
